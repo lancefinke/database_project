46bcfc8dd9cac61ea6966d2d87f9df0e163c66ad
Lance <lancefinke@icloud.com>
2025-02-06 16:31:25 -0600

    fix: better colors and formatting

--- my-profile-app/src/App.jsx
+++ my-profile-app/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'; // Only import useState if you plan to use it
 import ProfilePage from './ProfilePage'; // Ensure the path is correct
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from './HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='profile'element={<ProfilePage />} />
+          <Route path='/' element={<HomePage />} />
+          <Route path='/home' element={<HomePage />} />
+          <Route path='profile' element={<ProfilePage />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ my-profile-app/src/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import SongPost from "./HomePageComponents/SongPost";
+import './homePage.css'
+
+const HomePage = ()=>{
+    const songflags= ["Rap","HipHop"];
+
+    return(
+         <>
+            <div className="song-list"><SongPost name="All of the Lights" creator="Kanye West" duration= "5:26" flags={songflags} iconImage="https://i.ytimg.com/vi/HAfFfqiYLp0/maxresdefault.jpg"/></div>
+         </>
+    );
+}
+
+
+export default HomePage;
