5f2db9ea2e975ceeb03f94753ed0d262ed4bec92
Lance <lancefinke@icloud.com>
2025-02-25 15:35:53 -0600

    added search bar

--- my-profile-app/src/SearchPage/SearchPage.jsx
+++ my-profile-app/src/SearchPage/SearchPage.jsx
@@ -11,10 +11,23 @@ const SearchPage = () => {
 
   return (
     <div className="search-container">
+
+      <h1>Search Music</h1>
+      <p>Find your favorite songs and artists.</p>
+
+
       <div className="search-bar-wrapper">
             <div className="search-icon"><span className="icon-align">🔎︎</span></div>
-            <input className="search-bar" type="search" value={searchText} placeholder="Enter a Song name or Artist..." onChange={(e)=>{setSearchText(e.target.value)}}></input>
+            <input className="search-bar" 
+                    type="search" value={searchQuery} 
+                    placeholder="Search for Artist, song, or Album" 
+                    onChange={(e)=>{setSearchQuery(e.target.value)}}>
+            </input>
+
+            <button className="search-button" onClick={handleSearch}>Search</button>
         </div>
+
+  
       
       <div className="search-results">
         <h2>Search Results</h2>
