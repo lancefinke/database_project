690367a46da3173620728691ba64150f19bd2769
Jose <perlajose833@gmail.com>
2025-03-12 00:41:00 -0500

    start sql connections

--- backend/MusicLibraryBackend/obj/Debug/net8.0/MusicLibraryBackend.AssemblyInfo.cs
+++ backend/MusicLibraryBackend/obj/Debug/net8.0/MusicLibraryBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicLibraryBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+def63debfea252bbcd4e1c50850befc26a795f7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60e2a9d4c1c253f5d7d339fd36a264313db41f69")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicLibraryBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicLibraryBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
