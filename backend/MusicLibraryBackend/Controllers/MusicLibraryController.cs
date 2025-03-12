60e2a9d4c1c253f5d7d339fd36a264313db41f69
Jose <perlajose833@gmail.com>
2025-03-12 00:38:20 -0500

    starting sql connections

--- backend/MusicLibraryBackend/Controllers/MusicLibraryController.cs
+++ backend/MusicLibraryBackend/Controllers/MusicLibraryController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
+//TODO CREATE CONNECTIONS AND QUERIES TO DATABASE
 namespace MusicLibraryBackend.Controllers
 {
     [Route("api/[controller]")]
