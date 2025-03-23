603f79cc0e4b979dcc427a6cd63ee10e31fda851
Jose <perlajose833@gmail.com>
2025-03-23 11:05:50 -0500

    fixed backend & get api to database works

---
+++ backend/MusicLibraryBackend/Controllers/UsersController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MusicLibraryBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        public UsersController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET: api/<UsersController>
+        [HttpGet]
+        [Route("GetUsers")]
+        public JsonResult GetUsers()
+        {
+            string query = "select * from dbo.USERS";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("DefaultConnection");
+            SqlDataReader myReader;
+            using(SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query,myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/<UsersController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // TODO:::: POST api/<UsersController> 
+        //[HttpPost]
+        //[Route("login")]
+        //public IActionResult 
+        //public JsonResult CreateUser([From])
+        //public void Post([FromBody] string value)
+        //{
+        }
+
+        //// PUT api/<UsersController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<UsersController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
